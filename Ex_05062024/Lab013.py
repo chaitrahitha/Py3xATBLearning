da9b6273726398f5b0772d97c4d83fbcbfb74b4f
Chaitra <chaitrabn.shetty1@gmail.com>
2024-06-11 12:42:08 +0530

    Initial Commit

--- Ex_05062024/Lab013.py
+++ Ex_05062024/Lab013.py
@@ -2,3 +2,6 @@
 first_name = input("Enter your First name")
 last_name = input("Enter your Last name")
 print("Your first name is ", first_name, "and your last name is ", last_name)
+list = [1,2,3,4]
+print()
+input()
